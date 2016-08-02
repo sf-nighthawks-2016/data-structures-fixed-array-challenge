f08a20377fb7d5f585ff135a66c64ec864ba619f
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2016-08-02 16:34:23 -0700

    add initialize method that takes size of an array and make size and attr accessor

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,10 @@
 class FixedArray
+	attr_accessor :size
 
+	def initialize(size=7)
+		@size = size
+		@array = Array.new(size)
+	end
+
+	
 end
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,4 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
 end
