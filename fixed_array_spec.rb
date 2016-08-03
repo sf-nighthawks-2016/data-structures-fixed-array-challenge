1885014dee829c20b171ef724c8226f805ac6b05
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2016-08-02 18:12:27 -0700

    add all the methods and wrote spec test for those methods

--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,21 +1,40 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-
+	let(:array) {FixedArray.new(9)}
 
 	describe "new" do
   		it "should return the default size" do
   			array = FixedArray.new
-  			p array
   			expect(array.size).to eq 7
 		end
 
 		it "should return the size you give it" do
-			size = 70
-			array =FixedArray.new(size)
-			expect(array.size).to eq 70
+			expect(array.size).to eq 9
 		end
+	end
 
+	describe "get" do 
+		it "returns the value at a specific position in the array" do
+     		 array = FixedArray.new
+      		expect(array.get(0)).to eq nil
+    	end
 
 	end
+
+	describe "set" do
+		it "should replace the index with element" do
+			array.set(4,"@")
+			expect(array.array).to eq [nil, nil, nil, nil, "@", nil, nil,nil,nil] 
+		end
+
+		it "should  raise error " do
+			expect(array.set(70,"@")).to eq "You out of bounds"
+		end
+
+		it "should  raise error " do
+			expect(array.set(-1,"@")).to eq "You out of bounds"
+		end
+	end
+
 end
